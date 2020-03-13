f52424acb9f5e1e3471b61b315efb9f1d623368a
brant <sjwu1234@gmail.com>
2020-03-13 11:10:06 +0800

    flutter端解析代码加上try catch

--- lib/flutter_current_locale.dart
+++ lib/flutter_current_locale.dart
@@ -8,18 +8,23 @@ class FlutterCurrentLocale {
       const MethodChannel('flutter_current_locale');
 
   static Future<Locale> get currentLocale async {
-    final List<String> locales = List<String>.from(await _channel.invokeMethod('current_locale'));
-    if (locales.length == 2 || locales.length > 3) {
-      return Locale(locales[0], locales[1]);
-    } else if (locales.length == 3) {
-      if (locales[2] == '' || locales[1] == '') {
-        return Locale(locales[0], locales[2]);
-      }
+    final List<String> locales =
+        List<String>.from(await _channel.invokeMethod('current_locale'));
+    try {
+      if (locales.length == 2 || locales.length > 3) {
+        return Locale(locales[0], locales[1]);
+      } else if (locales.length == 3) {
+        if (locales[2] == '' || locales[1] == '') {
+          return Locale(locales[0], locales[2]);
+        }
 
-      return Locale.fromSubtags(
-          languageCode: locales[0],
-          scriptCode: locales[1],
-          countryCode: locales[2]);
+        return Locale.fromSubtags(
+            languageCode: locales[0],
+            scriptCode: locales[1],
+            countryCode: locales[2]);
+      }
+    } catch (e) {
+      return Locale('en', '');
     }
 
     return null;
